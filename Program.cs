0c616372947275a3be7f5eb725cec44415b1e385
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-03 01:37:49 +0330

    design Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello,Welcome to sequential digits app");
+            Console.WriteLine("Enter your low number : ");
+            int low = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your high number : ");
+            int high = Convert.ToInt32(Console.ReadLine());
+            sequentialDigits sequentialDigits = new();
+            List<int> digits = sequentialDigits.sequentiallist(low , high);
+            string result = string.Join(",", digits);
+            if(digits.Count!=0)
+             Console.WriteLine($"Your sequential list between {low} and {high} is {result}");
+            else
+                Console.WriteLine("Something wrong...");
+
         }
     }
 }
