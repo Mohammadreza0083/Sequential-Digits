b4dc6c2af3349b8350dff30055767812e748cf5e
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-03 01:37:16 +0330

    solve loop problem in sequentiallist method

--- sequentialDigits.cs
+++ sequentialDigits.cs
@@ -1,18 +1,29 @@
 ﻿
+using System.Globalization;
+
 namespace Sequential_Digits
 {
     internal class sequentialDigits : IsequentialDigits
     {
         public List<int>? sequentiallist(int low, int high)
         {
-            List<int>? sequentiallist = new();
-            for (int i = low; i <= high; i++)
+            List<int>? sequentialList = new();
+            for (int i = 1; i < 9; i++)
             {
-                int sequentialDigits = ((i * 10) + ((i % 10) + 1));
-                if (sequentialDigits <= high)
-                    sequentiallist.Add(sequentialDigits);
+                int num = i;
+                for (int j = i+1; j <= 9; j++)
+                {
+                    num = (num * 10) + (j % 10);
+                    if (num > high)
+                        j = high + 1;
+                    if (num>=low && num<=high)
+                    {
+                        sequentialList.Add(num);
+                    }
+                }
             }
-            return sequentiallist;
+            return sequentialList;
+
         }
     }
 }
