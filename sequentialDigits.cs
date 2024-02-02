7cd77ef1c95c5d35ace4d116e9adb2f6bdab3368
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-03 00:31:01 +0330

    sequential class

---
+++ sequentialDigits.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Sequential_Digits
+{
+    internal class sequentialDigits : IsequentialDigits
+    {
+        public List<int>? sequentiallist(int low, int high)
+        {
+            List<int>? sequentiallist = new();
+            for (int i = low; i <= high; i++)
+            {
+                int sequentialDigits = ((i * 10) + ((i % 10) + 1));
+                if (sequentialDigits <= high)
+                    sequentiallist.Add(sequentialDigits);
+            }
+            return sequentiallist;
+        }
+    }
+}
